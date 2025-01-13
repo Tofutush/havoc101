49ebde71483fe381df3f50a9dece0a42668fe8ea
Tofutush <xiexuanqisirocco@163.com>
2025-01-13 18:09:14 +0800

    more styles

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,9 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [
-    Component.MobileOnly(Component.Explorer())
-  ],
+  afterBody: [],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
@@ -33,8 +31,9 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
+    Component.TableOfContents(),
     Component.Backlinks(),
+    Component.MobileOnly(Component.Explorer())
   ],
 }
 
